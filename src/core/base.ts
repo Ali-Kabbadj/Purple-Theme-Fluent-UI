8a17cd3f37dc2fce559a02d4e775069d7b0241ce
ali kabbadj <alikabbadj1994@gmail.com>
2025-05-23 11:50:44 +0100

    small code cleanup

--- src/core/base.ts
+++ src/core/base.ts
@@ -1,17 +1,20 @@
 import * as vscode from "vscode";
 import { registerCommands } from "./actions/Registrator";
 import { globals } from "./lib/globales";
-import { createLogger } from "../utils/logger";
+import { THEME_NAME } from "./lib/constants";
 
-export function activate_vscode_custom_ui_injector(
-  context: vscode.ExtensionContext,
-): void {
-  const logger = createLogger("activate_vscode_custom_ui_injector");
-
+export function init_extension(context: vscode.ExtensionContext): void {
   globals.init(context);
-  logger.config("globals", globals);
   registerCommands(context);
-  logger.config("extention fully initialized:");
 }
 
-export function deactivate_vscode_custom_ui_injector(): void {}
+export function deactivate_extension(): void {
+  const config = vscode.workspace.getConfiguration("workbench");
+  if (config.get<string>("colorTheme") === THEME_NAME) {
+    config.update(
+      "colorTheme",
+      "Default Dark Modern",
+      vscode.ConfigurationTarget.Global,
+    );
+  }
+}
--- src/extension.ts
+++ src/extension.ts
@@ -1,29 +1,11 @@
 import * as vscode from "vscode";
-import {
-  activate_vscode_custom_ui_injector,
-  deactivate_vscode_custom_ui_injector,
-} from "./core/base";
+import { init_extension, deactivate_extension } from "./core/base";
 import { THEME_NAME } from "./core/lib/constants";
 
 export async function activate(context: vscode.ExtensionContext) {
-  if (context.extensionMode === vscode.ExtensionMode.Development) {
-    console.log("Yeey!! Theme Editor Pro is live! 💜");
-    // await vscode.commands.executeCommand("workbench.action.toggleDevTools");
-  }
-
-  activate_vscode_custom_ui_injector(context);
+  init_extension(context);
 }
 
-// In extension.ts
 export function deactivate() {
-  // Add cleanup logic
-  const config = vscode.workspace.getConfiguration("workbench");
-  if (config.get<string>("colorTheme") === THEME_NAME) {
-    config.update(
-      "colorTheme",
-      "Default Dark Modern",
-      vscode.ConfigurationTarget.Global,
-    );
-  }
-  deactivate_vscode_custom_ui_injector();
+  deactivate_extension();
 }
